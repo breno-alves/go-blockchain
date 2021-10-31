349a400d39ebf99296c1541753ca581b04398c5a
Breno <breno550@hotmail.com>
2021-10-31 15:15:34 -0300

    feat: adding proof of work

--- main.go
+++ main.go
@@ -1,58 +1,26 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+	"strconv"
+
+	"github.com/breno-alves/go-blockchain/blockchain"
 )
 
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-}
-
-type BlockChain struct {
-	Blocks []*Block
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256((info))
-	b.Hash = hash[:]
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
-	return block
-}
-
-func (chain *BlockChain) addBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, new)
-}
-
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-func InitiBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
-}
-
 func main() {
-	chain := InitiBlockChain()
+	chain := blockchain.InitiBlockChain()
 
-	chain.addBlock("First block")
-	chain.addBlock("Second block")
-	chain.addBlock("Third block")
+	chain.AddBlock("First block")
+	chain.AddBlock("Second block")
+	chain.AddBlock("Third block")
 
 	for _, block := range chain.Blocks {
 		fmt.Printf("Previous hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n\n", block.Hash)
 
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
